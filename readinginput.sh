fee2d14160e8e31baca75ebc590f1a9d36784e0d
venkatesh <venkateshtadiboina123.com>
2025-05-20 10:15:11 +0530

    practice file to commit sucess

--- readinginput.sh
+++ readinginput.sh
@@ -13,33 +13,33 @@ LOG_FILE="$LOGS_DIR/$SCRIPTNAME.log"
 
 # Create directory for logs
 mkdir  -p $LOGS_DIR
- echo "Script start time: $(date)" | tee -a $LOG_FILE
+ echo "Script start time: $(date)" | tee -a $LOGS_DIR
 
 # List of packages to install
-PACKAGE_LIST=("mangodb" "python3" "nginx") | tee -a $LOG_FILE
+PACKAGE_LIST=("mangodb" "python3" "nginx") | tee -a $$LOGS_DIR
 
 # Function to validate package installation
 VALIDATE() 
 {
     if [ $? -eq 0 ]; then
 
-        echo "$1 SUCCESS" | tee -a $LOG_FILEz
+        echo "$1 SUCCESS" | tee -a $LOGS_DIR
     else
-        echo "$1 FAILURE" | tee -a $LOG_FILE
+        echo "$1 FAILURE" | tee -a $LOGS_DIR
         exit 1
     fi
 }
 
 # Loop through package list
 for PACKAGE in "${PACKAGE_LIST[@]}"; do 
-    dnf list installed "$PACKAGE" | tee -a $LOG_FILE
+    dnf list installed "$PACKAGE" | tee -a $LOGS_DIR
     
     if [ $? -ne 0 ]; then
-        echo "$PACKAGE not installed, attempting to install..." | tee -a $LOG_FILE
-        dnf install "$PACKAGE" -y | tee -a $LOG_FILE
+        echo "$PACKAGE not installed, attempting to install..." | tee -a $LOGS_DIR
+        dnf install "$PACKAGE" -y | tee -a $LOGS_DIR
         VALIDATE "$PACKAGE"
     else
-        echo "$PACKAGE is already installed" | tee -a $LOG_FILE
+        echo "$PACKAGE is already installed" | tee -a $LOGS_DIR
         exit 1
     fi
 done
