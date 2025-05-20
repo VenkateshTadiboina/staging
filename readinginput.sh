a79708b315a019bba4b0bfb5ee12377c7d74e62b
venkatesh <venkateshtadiboina123.com>
2025-05-20 10:52:49 +0530

    practice file to commit sucess

--- readinginput.sh
+++ readinginput.sh
@@ -13,19 +13,19 @@ LOG_FILE="$LOGS_DIR/$SCRIPTNAME.log"
 
 # Create directory for logs
 mkdir  -p $LOGS_DIR
- echo "Script start time: $(date)"   &>>$LOG_FILE
+ echo "Script start time: $(date)"   |tee -a $LOG_FILE
 
 # List of packages to install
-PACKAGE_LIST=("mysql" "python3" "nginx") &>>$LOG_FILE
+PACKAGE_LIST=("mysql" "python3" "nginx") |tee -a $LOG_FILE
 
 # Function to validate package installation
 VALIDATE() 
 {
     if [ $? -eq 0 ]; then
 
-        echo "$1 SUCCESS" &>>$LOG_FILE
+        echo "$1 SUCCESS" |tee -a $LOG_FILE
     else
-        echo "$1 FAILURE" &>>$LOG_FILER
+        echo "$1 FAILURE" |tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -36,10 +36,10 @@ do
     dnf list installed "$PACKAGE"
     
     if [ $? -ne 0 ]; then
-        echo "$PACKAGE not installed, attempting to install..." &>>$LOG_FILE
+        echo "$PACKAGE not installed, attempting to install..." |tee -a $LOG_FILE
         dnf install "$PACKAGE" -y 
         VALIDATE "$PACKAGE"
     else
-        echo "$PACKAGE is already installed" &>>$LOG_FILE
+        echo "$PACKAGE is already installed" |tee -a $LOG_FILE
     fi
 done
