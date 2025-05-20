e4942dd20e3fb32fdbe9b85ffc0904f6c0db540e
venkatesh <venkateshtadiboina123.com>
2025-05-20 10:32:46 +0530

    practice file to commit sucess

--- readinginput.sh
+++ readinginput.sh
@@ -13,33 +13,32 @@ LOG_FILE="$LOGS_DIR/$SCRIPTNAME.log"
 
 # Create directory for logs
 mkdir  -p $LOGS_DIR
- echo "Script start time: $(date)" | tee -a $LOGS_DIR
+ echo "Script start time: $(date)" 
 
 # List of packages to install
-PACKAGE_LIST=("mangodb" "python3" "nginx") | tee -a $$LOGS_DIR
+PACKAGE_LIST=("mangodb" "python3" "nginx") 
 
 # Function to validate package installation
 VALIDATE() 
 {
     if [ $? -eq 0 ]; then
 
-        echo "$1 SUCCESS" | tee -a $LOGS_DIR
+        echo "$1 SUCCESS" 
     else
-        echo "$1 FAILURE" | tee -a $LOGS_DIR
+        echo "$1 FAILURE" 
         exit 1
     fi
 }
 
 # Loop through package list
 for PACKAGE in "${PACKAGE_LIST[@]}"; do 
-    dnf list installed "$PACKAGE" | tee -a $LOGS_DIR
+    dnf list installed "$PACKAGE"
     
     if [ $? -ne 0 ]; then
-        echo "$PACKAGE not installed, attempting to install..." | tee -a $LOGS_DIR
-        dnf install "$PACKAGE" -y | tee -a $LOGS_DIR
+        echo "$PACKAGE not installed, attempting to install..." 
+        dnf install "$PACKAGE" -y 
         VALIDATE "$PACKAGE"
     else
-        echo "$PACKAGE is already installed" | tee -a $LOGS_DIR
-        exit 1
+        echo "$PACKAGE is already installed" 
     fi
 done
